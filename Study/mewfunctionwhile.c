3132fd871d122fb550948ee7659e4f77abf1833c
Gabriel <gabri.moraes.2011@gmail.com>
2025-12-15 15:38:47 -0300

    Small tweaks on the do... while program.

--- Study/mewfunctionwhile.c
+++ Study/mewfunctionwhile.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
-void meow(int n); //Declares the existing meow function before it's use
+int get_value(void);
+void meow(int n); 
+
 int main(void) 
 {
-    int n; //Declares the variable n inside the main, so it can "exist" inside or outside the loop.
-    do //Starts the loop do... while
-    {
-        printf("Number: "); //Displays a question for the user
-        scanf("%i", &n); //Gets user's input and stores in the n variable
-    }
-    while (n < 1); //Puts a condition for the loop to run.
-    meow(n); //Executes the function meow (n) times
+    int times = get_value();
+    meow(times); 
 }
 
-void meow(int n) //Declares the function structure
+int get_value(void)
 {
-    for (int i = 0; i < n; i++)
+    int n;
+    do
+    {
+        printf("Number: "); 
+        scanf("%i", &n); 
+    }
+    while (n < 1);
+    return n;
+}
+
+void meow(int a) 
+{
+    for (int i = 0; i < a; i++)
     {
         printf("meow\n");
     }
