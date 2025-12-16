403808a90ed9c4b45fee867fb56169006f28426b
Gabriel <gabri.moraes.2011@gmail.com>
2025-12-16 19:56:57 -0300

    Few updates on the calculator, prototype, included the string.h library

--- Study/calculator.c
+++ Study/calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h> //Lets the user use the string comparison tool.
 
 int get_y(void);
 int get_x(void);
@@ -6,15 +7,23 @@ int main(void)
 {
     int x = get_x();
     int y = get_y();
-    printf("The value of X is %i, while the value of Y is %i.\n", x, y);
+    char op [4];
+    printf("The value of X is %i, while the value of Y is %i.\n", x, y); 
     printf("Which operation do you choose? (sum, dif, mul, div)?");
+    scanf("%s", op);
+    if (op == "sum")
+    {
+        int n = x + y;
+        printf("%i", n);
+    }
+    
 }
 
 int get_x(void)
 {
     int n;
     printf("Value of X: ");
-    scanf("%i,", &n);
+    scanf("%i", &n);
     return n;
 }
 int get_y(void)
