33fd19fed86ed086ef54baf93a1c65a9f51ebc42
Gabriel <gabri.moraes.2011@gmail.com>
2025-12-23 22:24:22 -0300

    Small tweaks on the string encrypter project.

--- Study/string.c
+++ Study/string.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+int encrypt(void);
+char name[15];
+int N = 10001;
+int i;
 int main(void)
 {
-    char name[15];
     printf("What's your name? ");
     scanf("%s", name);
     printf("Hello!, %s\n", name);
     printf("First letter: %c \n", name[0]);
-    int N = 10001;
-    int i;
+    encrypt();
+    printf("Cryptic name: %s", name);
+}
+
+int encrypt(void)
+{
     for (i = 0; i < strlen(name); i++)
     {
         name[i] = name[i] + N;
     }
-    printf("Cryptic name: %s", name);
 }
